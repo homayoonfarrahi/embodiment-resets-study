ccd984f0873dd96a612872f9fe34bc4039008840
Homayoon Farrahi <farrahi@ualberta.ca>
2025-12-05 17:29:54 -0700

    add requirements file and package structure

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='robot',
+      description='Embodiment Resets Study',
+      author='Homayoon Farrahi',
+      author_email='farrahi@ualberta.ca',
+      version='0.0.1')
